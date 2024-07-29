7cf408b4c106b703c3db1286a029499bfc799e37
mfachrizalg <mohammad.fachrizal123@gmail.com>
2024-07-29 22:32:51 +0700

    feat : update and status code

--- mobile/api/controllers/userController.js
+++ mobile/api/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const BankSampah = require('../models/BankSampah');
+const Notification = require('../models/Notification');
 const bcrypt = require('bcryptjs');
 
 exports.registerUser = async (req, res) => {
@@ -12,7 +13,7 @@ exports.registerUser = async (req, res) => {
         // kalo user sudah ada
         if (user) return res.status(400).json({ message: "User already exists" });
         // kalo bank sampah tidak ada
-        if (!banksampah) return res.status(400).json({ message: "Bank Sampah not found" });
+        if (!banksampah) return res.status(404).json({ message: "Bank Sampah not found" });
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = new User({ 
             username, fullname, phoneNumber, password: hashedPassword, role , bankSampah: banksampah._id
@@ -20,6 +21,29 @@ exports.registerUser = async (req, res) => {
         await newUser.save();
         await BankSampah.findByIdAndUpdate(banksampah._id, { $push: { users: newUser._id } });
         res.status(201).json({ message: "User registered successfully" });
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+exports.updateUser = async (req,res) => {
+    try {
+        const { username, fullname, phoneNumber } = req.body;
+        if (!username || !fullname || !phoneNumber) return res.status(400).json({ message: "Please fill all fields" });
+        const updatedUser = await User.findByIdAndUpdate(req.user.id, { username, fullname, phoneNumber}, { new: true });
+        if (updatedUser) {
+            const notification = new Notification({
+                title: "Pengaturan Profil Berhasil",
+                message: "Profil berhasil diperbaharui",
+                date : Date.now() + 7*60*60*1000,
+                type : "profile",
+                user: new mongoose.Types.ObjectId(req.user.id)
+            });
+            await notification.save();
+            return res.status(200).json(updatedUser);
+        }
+        else return res.status(404).json({ message: "User not found" });
     }
     catch (error) {
         res.status(500).json({ message: error.message });
