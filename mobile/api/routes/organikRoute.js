02be2edc4f8ed530f47f1362d881d5cd73b250a4
mfachrizalg <mohammad.fachrizal123@gmail.com>
2024-08-12 22:43:29 +0700

    fix alex ngga marah lagi

--- mobile/api/routes/organikRoute.js
+++ mobile/api/routes/organikRoute.js
@@ -1,12 +1,13 @@
 const { createOrganik, getLinkImage, verifyOrganik, riwayatOrganik} = require('../controllers/organikController');
 const { protectAdminOrganik, protectOrganik } = require('../middleware/checkRole');
-const convertToBuffer = require('../middleware/convertToBuffer');
+const multer = require('multer');
+const upload = multer()
 const express = require('express');
 const router = express.Router();
 
 router.route('/')
     .get(protectOrganik, riwayatOrganik)
-    .post(protectOrganik, convertToBuffer.any(), createOrganik);
+    .post(protectOrganik, upload.any(), createOrganik);
 router.route('/:id')
     .get(protectAdminOrganik, getLinkImage)
     .patch(protectAdminOrganik, verifyOrganik);
